eab4aa57cad763817f51853526c3de10b994adcb
ahmmadSumon <arifahmmadsumon@gmail.com>
2025-01-11 20:33:46 +0600

    fix

--- services/authservice.ts
+++
@@ -1,9 +0,0 @@
-import api from "./api";
-
-export const registerUser = (data: { name: string; email: string; password: string }) => {
-  return api.post("/auth/register", data);
-};
-
-export const loginUser = (data: { email: string; password: string }) => {
-  return api.post("/auth/login", data);
-};
--- services/flightService.ts
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export const getFlights = (query: string) => api.get(`/flight?${query}`);
-export const createFlight = (data: any) => api.post("/flight/create", data);
-export const getFlightById = (id: string) => api.get(`/flight/${id}`);
